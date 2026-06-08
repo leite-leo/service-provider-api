cd4f7bc33ed05b91afd6443e800a39f75a8cba09
Leo Leite <leoinatel@gmail.com>
2026-06-08 19:14:41 -0300

    refactor(middleware): map sequelize unique constraint errors to conflict

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { UniqueConstraintError } = require('sequelize');
 const { AppError } = require('../utils/errors.utils');
 
 // Express identifies error handlers by 4-arg signature; _next is required even when unused
@@ -7,6 +8,16 @@ module.exports = (err, req, res, _next) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       error: { code: err.code, message: err.message, details: err.details },
+    });
+  }
+
+  if (err instanceof UniqueConstraintError) {
+    const field = err.errors?.[0]?.path || 'value';
+    return res.status(409).json({
+      error: {
+        code: 'CONFLICT',
+        message: `A resource with this ${field} already exists`,
+      },
     });
   }
 
